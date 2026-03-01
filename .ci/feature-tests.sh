540f7020627dec4f836b731ac3fb50b3b00ed87a
Philipp <heydraundphilipp@gmail.com>
2026-03-01 15:30:18 +0100

    Update feature tests to use 1.1.1.1 instead of cloudflare.com for consistency & for workaround since ipv6 rn not working

--- .ci/feature-tests.sh
+++ .ci/feature-tests.sh
@@ -28,16 +28,16 @@ fi
 
 OUTPUT1=$($MEOWPING_PATH 1.1.1.1 -c 1 -m -p 53)
 if ! echo "$OUTPUT1" | sed 's/\x1b\[[0-9;]*m//g' | grep -q "Cloudflare"; then
-    echo "Test failed: Expected output to contain 'Cloudflare'"
+    echo "Test failed: Expected output to contain '1.1.1.1'"
     echo "Actual output:"
     echo "$OUTPUT1"
     exit 1
 fi
 
 MEOWPING="$MEOWPING_PATH"
-OUTPUT2=$($MEOWPING https://cloudflare.com -c 1 -m -p 443)
-if ! echo "$OUTPUT2" | sed 's/\x1b\[[0-9;]*m//g' | grep -q "AS13335 Cloudflare, Inc"; then
-    echo "Test failed: Expected output to contain 'AS13335 Cloudflare, Inc' for https://cloudflare.com"
+OUTPUT2=$($MEOWPING https://1.1.1.1 -c 1 -m -p 443)
+if ! echo "$OUTPUT2" | sed 's/\x1b\[[0-9;]*m//g' | grep -q "AS13335 1.1.1.1, Inc"; then
+    echo "Test failed: Expected output to contain 'AS13335 1.1.1.1, Inc' for https://1.1.1.1"
     echo "Actual output:"
     echo "$OUTPUT2"
     exit 1
@@ -45,12 +45,12 @@ fi
 
 if [[ "$HAVE_ICMP_PERMS" == "true" ]]; then
     if [[ -n "$USE_SUDO_ICMP" ]]; then
-        OUTPUT3=$($USE_SUDO_ICMP $MEOWPING_PATH https://cloudflare.com -c 1 -m)
+        OUTPUT3=$($USE_SUDO_ICMP $MEOWPING_PATH https://1.1.1.1 -c 1 -m)
     else
-        OUTPUT3=$($MEOWPING_PATH https://cloudflare.com -c 1 -m)
+        OUTPUT3=$($MEOWPING_PATH https://1.1.1.1 -c 1 -m)
     fi
     if ! echo "$OUTPUT3" | sed 's/\x1b\[[0-9;]*m//g' | grep -q "Reply from"; then
-        echo "Test failed: Expected output to contain 'Reply from' for https://cloudflare.com"
+        echo "Test failed: Expected output to contain 'Reply from' for https://1.1.1.1"
         echo "Actual output:"
         echo "$OUTPUT3"
         exit 1
